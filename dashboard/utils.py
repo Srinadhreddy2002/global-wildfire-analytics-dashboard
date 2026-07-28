15cf325d46850ab0908f40911d9193041cff7201
srinadh reddy <srinadhreddy@srinadhs-MacBook-Air.local>
2026-07-29 00:50:01 +0200

    Fixed dataset path

--- dashboard/utils.py
+++ dashboard/utils.py
@@ -1,13 +1,16 @@
+import os
 import pandas as pd
 import streamlit as st
 
 @st.cache_data
 def load_data():
 
-    df = pd.read_csv("data/wildfire_data.csv")
+    BASE_DIR = os.path.dirname(__file__)
+    csv_path = os.path.join(BASE_DIR, "data", "wildfire_data.csv")
+
+    df = pd.read_csv(csv_path)
 
     df["acq_date"] = pd.to_datetime(df["acq_date"])
-
     df["year"] = df["acq_date"].dt.year
     df["month"] = df["acq_date"].dt.month_name()
 
