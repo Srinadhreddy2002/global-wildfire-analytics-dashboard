d5ccacefe341029307555d2f8eb23d3b453a5c8c
srinadh reddy <srinadhreddy@srinadhs-MacBook-Air.local>
2026-07-29 00:57:00 +0200

    Added month_name column

--- dashboard/utils.py
+++ dashboard/utils.py
@@ -4,14 +4,15 @@ import streamlit as st
 
 @st.cache_data
 def load_data():
-
     BASE_DIR = os.path.dirname(__file__)
     csv_path = os.path.join(BASE_DIR, "data", "wildfire_data.csv")
 
     df = pd.read_csv(csv_path)
 
     df["acq_date"] = pd.to_datetime(df["acq_date"])
+
     df["year"] = df["acq_date"].dt.year
-    df["month"] = df["acq_date"].dt.month_name()
+    df["month"] = df["acq_date"].dt.month
+    df["month_name"] = df["acq_date"].dt.month_name()
 
     return df
